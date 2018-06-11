cd36bb0b4dfdb863458191eaf54f6cae32d38b9b
HendrikBD <hendrik.danen@gmail.com>
2018-06-11 14:18:15 -0400

    Adding event listeners to menu & back button for sliding sidebar

--- hopper.js
+++ hopper.js
@@ -1,3 +1,11 @@
 (function() {
-  console.log("yo");
+
+  document.querySelector(".hamburger").addEventListener("click", function(){
+    document.querySelector(".sidebar").classList.add("open");
+  })
+  
+  document.querySelector(".sidebar .header .back").addEventListener("click", function(){
+    document.querySelector(".sidebar").classList.remove("open");
+  })
+
 })();

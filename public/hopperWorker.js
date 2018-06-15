79b89359dc5fa5cf31e19289fc148f0bd4871518
HendrikBD <hendrik.danen@gmail.com>
2018-06-15 11:28:11 -0400

    Adding service worker and listing files to cache

---
+++ public/hopperWorker.js
@@ -0,0 +1,11 @@
+var cacheName = "hopperCache";
+var filesToCache = [
+  '/',
+  '/hopper.html',
+  '/scripts/hopper.js',
+  '/stylesheets/hopper.css',
+  '/img/backArrow.svg',
+  '/img/home.png',
+  '/img/plus.png',
+  '/img/rssFeed.png'
+];
